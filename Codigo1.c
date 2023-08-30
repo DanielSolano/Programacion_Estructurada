21fb5c9aa574796f7a344c997fdaaffeae44f3c3
DanielSolano <solanomezaangel@gmail.com>
2023-08-29 17:01:23 -0700

    modified:   Codigo1.c 	new file:   output/Codigo1.exe

--- Codigo1.c
+++ Codigo1.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 int main()
 {
-    int num1, num2, opc, resultado;
+    int num1, num2, opc;
     printf("---MENU---\n1.-Suma\n2.-Resta\n3.-Multiplicacion\n4.-Division\n");
     printf("Elige una opcion: ");
-    scanf("%d",opc);
+    scanf("%d",&opc);
     printf("Numero 1: ");
     scanf("%d",&num1);
     printf("Numero 2: ");
-    scanf("%d",&num2)
+    scanf("%d",&num2);
     if(opc==1)
     {
-
+        printf("La suma es: %d",num1+num2);
     }
     else
     {
         if(opc==2)
         {
-
+            printf("La resta es: %d",num1-num2);
         }
         else
         {
             if(opc==3)
             {
-
+                printf("La multiplicacion es: %d",num1*num2);
             }
             else
             {
                 if(opc==4)
                 {
-
+                    printf("La division es: %.2f",(num1+0.0)/(num2+0.0));
                 }
                 else
                 {
