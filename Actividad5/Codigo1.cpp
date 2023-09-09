98dc9d2cb43ba45708832cb43151df3bdb602696
DanielSolano <solanomezaangel@gmail.com>
2023-09-09 00:18:05 -0700

    modified:   Actividad5/Codigo1.cpp 	modified:   Actividad5/output/Codigo1.exe

--- Actividad5/Codigo1.cpp
+++ Actividad5/Codigo1.cpp
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 10
+#include <time.h>
+#define N 35
 //*** PROTOTIPOS DE FUNCIONES  ******
 int msges();
 void menu();
 void Descendentes(void);
-void Factorial(void);
-void Digitos(void);
+void Pares(void);
+void MeMa(void);
+void Multiplicar(void);
 //****  main principal  *********
 int main()
 {
+    srand(time(NULL));
     menu();
 
     return 0;
@@ -22,8 +25,9 @@ int msges()
     system("CLS");
     printf("   M  E   N   U \n");
     printf("1.- DESCENDENTES \n");
-    printf("2.- FACTORIAL \n");
-    printf("3.- DIGITOS \n");
+    printf("2.- PARES \n");
+    printf("3.- MAYOR Y MENOR \n");
+    printf("4.- TABLAS DE MULTIPLICAR \n");
     printf("0.- SALIR  \n");
     printf("ESCOGE UNA OPCION: ");
     scanf("%d", &op);
@@ -42,10 +46,13 @@ void menu()
             Descendentes();
             break;
         case 2:
-            Factorial();
+            Pares();
             break;
         case 3:
-            Digitos();
+            MeMa();
+            break;
+        case 4:
+            Multiplicar();
             break;
         }
 
@@ -56,39 +63,95 @@ void menu()
 void Descendentes(void)
 {
     //  VARIALES LOCALES
-    int numero, i, j, temp;
+    int numero, i;
     system("CLS");
     //  AQUI DESARROLLO PROGRAMA
     printf("   DESCENDENTES\n");
     printf("Dame un numero: \n");
     scanf("%d", &numero);
     i = 0;
-    temp = numero;
     system("CLS");
-    for (i = numero - 1; i >= 0; i--)
+    for (i = numero; i > 0; i--) // Se guarda con el valor del numero para imprimirlo y decrementarlo cada iteracion
     {
-        printf("%d\n", temp);
-        temp--;
+        printf("%d\n", i);
     }
     system("PAUSE");
 }
 
 //*************************
-void Factorial(void)
+void Pares(void)
 {
 
     //  VARIALES LOCALES
+    int i, numero, n_par, n_impar, par, impar;
     system("CLS");
     //  AQUI DESARROLLO PROGRAMA
-    printf("   FACTORIAL\n");
+    i = 0;
+    n_par = 0;
+    n_impar = 0;
+    par = 0;
+    impar = 0;
+    printf("   PARES\n");
+    for (i = 0; i < 40; i++)
+    {
+        numero = rand() % 201; // Genera un numero entre 0 y 200
+        if (numero % 2 == 0)
+        {
+            n_par++;
+            par = numero + par;
+            printf("El numero %d es par\n", numero);
+        }
+        else
+        {
+            n_impar++;
+            impar = numero + impar;
+            printf("El numero %d es impar\n", numero);
+        }
+    }
+    printf("\n%d numeros pares\n%d numeros impares\nLa suma de los numeros pares es: %d\nLa suma de los numeros impares es: %d\n", n_par, n_impar, par, impar);
+
     system("PAUSE");
 }
 //****************************
-void Digitos(void)
+void MeMa(void)
 {
     //  VARIALES LOCALES
+    int mayor, menor, numero, i;
     system("CLS");
     //  AQUI DESARROLLO PROGRAMA
-    printf("   DIGITOS\n");
+    printf("   MAYOR Y MENOR\n");
+    numero = rand() % 101 + 100; // Genera un numero entre 100 y 200
+    menor = numero;
+    mayor = numero;
+    for (i = 0; i < N - 1; i++)
+    {
+        numero = rand() % 101 + 100;
+        if (numero > mayor)
+        {
+            mayor = numero; // Convierte al mayor
+        }
+        if (numero < menor)
+        {
+            menor = numero; // Convierte al menor
+        }
+    }
+    printf("El numero mayor es %d\nEl menor es %d\n", mayor, menor);
+    system("PAUSE");
+}
+//****************************
+void Multiplicar(void)
+{
+    //  VARIALES LOCALES
+    int numero, i;
+    system("CLS");
+    //  AQUI DESARROLLO PROGRAMA
+    printf("   TABLA DE MULTIPLICAR\n");
+    printf("Desplegar tabla del: \n(Numero del 1-20)\n");
+    scanf("%d", &numero);
+    system("CLS");
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", numero, i, (numero * i));
+    }
     system("PAUSE");
 }
