496f5b4fbc9dd3431344116bc79a745fb27c9b79
DanielSolano <solanomezaangel@gmail.com>
2023-09-17 20:28:43 -0700

    modified:   Actividad6/ADSM_ACT6_02.c 	new file:   Actividad6/output/ADSM_ACT6_02.exe

--- Actividad6/ADSM_ACT6_02.c
+++ Actividad6/ADSM_ACT6_02.c
@@ -4,82 +4,103 @@
 //*** PROTOTIPOS DE FUNCIONES  ******
 int msges();
 void menu();
-void Fibonacci (void);
-void Factorial (void);
-void Digitos (void);
+void derechoExamen(void);
+void Factorial(void);
+void Digitos(void);
 //****  main principal  *********
 int main()
 {
-   menu();
+    menu();
 
-	return 0;
+    return 0;
 }
 //  *** DESARROLLO DE LAS FUNCIONES  ******
 //*********************
 int msges()
-{ int op;
-  system ("CLS");
-  printf ("   M  E   N   U \n");
-  printf("1.- FIBONACCI \n");
-  printf("2.- FACTORIAL \n");
-  printf("3.- DIGITOS \n");
-  printf("0.- SALIR  \n");
-  printf("ESCOGE UNA OPCION: ");
-  scanf ("%d",&op);
-  return op;
+{
+    int op;
+    system("CLS");
+    printf("   M  E   N   U \n");
+    printf("1.- EXAMEN DE NIVELACION \n");
+    printf("2.- FACTORIAL \n");
+    printf("3.- DIGITOS \n");
+    printf("0.- SALIR  \n");
+    printf("ESCOGE UNA OPCION: ");
+    scanf("%d", &op);
+    return op;
 }
 //****************
 void menu()
 {
-  int op;
-  do{
-      op=msges();
-      switch (op)
-      {
+    int op;
+    do
+    {
+        op = msges();
+        switch (op)
+        {
         case 1:
-              Fibonacci();
-              break;
+            derechoExamen();
+            break;
         case 2:
-              Factorial();
-              break;
+            Factorial();
+            break;
         case 3:
-              Digitos();
-              break;
+            Digitos();
+            break;
+        }
 
-      }
-
-    }while (op != 0);
-
-
+    } while (op != 0);
 }
 
 //*********************
-void Fibonacci (void)
+void derechoExamen(void)
 {
-    //  VARIALES LOCALES 
-    system ("CLS");
+    //  VARIABLES LOCALES
+    int i, j, prom, s_derecho, cal;
+    system("CLS");
     //  AQUI DESARROLLO PROGRAMA
-    printf("   FIBONACCI\n"); 
-    system ("PAUSE");
+    s_derecho = 0;
+    i = 1;
+    j = 1;
+    printf("   EXAMEN DE NIVELACION\n");
+    for (i = 1; i <= 40; i++)
+    {
+        prom = 0;
+        system("CLS");
+        printf("ALUMNO %d\n",i); 
+        for (j = 1; j <= 5; j++, prom += cal)
+        {
+            printf("Calificacion unidad %d: \n", j);
+            scanf("%d",&cal);
+        }
+        prom /= 5;
+        printf("%d",prom);
+        if(prom<50)
+        {
+            s_derecho++;
+        }
+    }
+    system("CLS");
+    printf("La cantidad de alumnos sin derecho a examen es: %d\n",s_derecho);
+    system("PAUSE");
 }
 
 //*************************
-void Factorial (void)
+void Factorial(void)
 {
 
-    //  VARIALES LOCALES 
-    system ("CLS");
+    //  VARIALES LOCALES
+    system("CLS");
     //  AQUI DESARROLLO PROGRAMA
-    printf("   FACTORIAL\n"); 
-    system ("PAUSE");
+    printf("   FACTORIAL\n");
+    system("PAUSE");
 }
 //****************************
-void Digitos (void)
+void Digitos(void)
 {
-    //  VARIALES LOCALES 
-    system ("CLS");
+    //  VARIALES LOCALES
+    system("CLS");
     //  AQUI DESARROLLO PROGRAMA
-    printf("   DIGITOS\n"); 
-    system ("PAUSE");
-
+    printf("   DIGITOS\n");
+    system("PAUSE");
 }
