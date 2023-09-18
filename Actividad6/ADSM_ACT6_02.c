91a45767c1631bd34be679a7fb7c228938059be1
DanielSolano <solanomezaangel@gmail.com>
2023-09-17 23:46:41 -0700

    modified:   Actividad6/ADSM_ACT6_02.c 	modified:   Actividad6/output/ADSM_ACT6_02.exe

--- Actividad6/ADSM_ACT6_02.c
+++ Actividad6/ADSM_ACT6_02.c
@@ -4,9 +4,10 @@
 //*** PROTOTIPOS DE FUNCIONES  ******
 int msges();
 void menu();
-void derechoExamen(void);
-void Factorial(void);
-void Digitos(void);
+void DerechoExamen(void);
+void Multiplicar(void);
+void Rango(void);
+void Embarcacion(void);
 //****  main principal  *********
 int main()
 {
@@ -22,8 +23,9 @@ int msges()
     system("CLS");
     printf("   M  E   N   U \n");
     printf("1.- EXAMEN DE NIVELACION \n");
-    printf("2.- FACTORIAL \n");
-    printf("3.- DIGITOS \n");
+    printf("2.- TABLAS DE MULTIPLICAR \n");
+    printf("3.- NUMEROS DENTRO DE RANGO \n");
+    printf("4.- EMBARCACION FINISTERRA \n");
     printf("0.- SALIR  \n");
     printf("ESCOGE UNA OPCION: ");
     scanf("%d", &op);
@@ -39,13 +41,16 @@ void menu()
         switch (op)
         {
         case 1:
-            derechoExamen();
+            DerechoExamen();
             break;
         case 2:
-            Factorial();
+            Multiplicar();
             break;
         case 3:
-            Digitos();
+            Rango();
+            break;
+        case 4:
+            Embarcacion();
             break;
         }
 
@@ -53,7 +58,7 @@ void menu()
 }
 
 //*********************
-void derechoExamen(void)
+void DerechoExamen(void)
 {
     //  VARIABLES LOCALES
     int i, j, prom, s_derecho, cal;
@@ -67,40 +72,134 @@ void derechoExamen(void)
     {
         prom = 0;
         system("CLS");
-        printf("ALUMNO %d\n",i); 
+        printf("ALUMNO %d\n", i);
         for (j = 1; j <= 5; j++, prom += cal)
         {
             printf("Calificacion unidad %d: \n", j);
-            scanf("%d",&cal);
+            scanf("%d", &cal);
         }
         prom /= 5;
-        printf("%d",prom);
-        if(prom<50)
+        printf("%d", prom);
+        if (prom < 50)
         {
             s_derecho++;
         }
     }
     system("CLS");
-    printf("La cantidad de alumnos sin derecho a examen es: %d\n",s_derecho);
+    printf("La cantidad de alumnos sin derecho a examen es: %d\n", s_derecho);
     system("PAUSE");
 }
 
 //*************************
-void Factorial(void)
+void Multiplicar(void)
 {
-
     //  VARIALES LOCALES
+    int i, j;
     system("CLS");
     //  AQUI DESARROLLO PROGRAMA
-    printf("   FACTORIAL\n");
+    printf("   TABLAS DE MULTIPLICAR\n");
+    for (i = 1; i <= 10; i++)
+    {
+        system("CLS");
+        printf("TABLA DEL %d\n", i);
+        for (j = 1; j <= 10; j++)
+        {
+            printf("%d x %d = %d\n", i, j, (i * j));
+        }
+        system("PAUSE");
+    }
+}
+//****************************
+void Rango(void)
+{
+    //  VARIALES LOCALES
+    int i, inferior, superior, n, suma, limite, numero;
+    system("CLS");
+    //  AQUI DESARROLLO PROGRAMA
+    printf("   NUMEROS DENTRO DE RANGO\n");
+    system("CLS");
+    printf("Rango inferior: \n");
+    scanf("%d", &inferior);
+    printf("Rango superior: \n");
+    scanf("%d", &superior);
+    if (inferior >= superior)
+    {
+        do
+        {
+            system("CLS");
+            printf("Ingresa un rango valido\n");
+            system("PAUSE");
+            printf("Rango inferior: \n");
+            scanf("%d", &inferior);
+            printf("Rango superior: \n");
+            scanf("%d", &superior);
+        } while (inferior >= superior);
+    }
+
+    limite = superior - inferior;
+    system("CLS");
+    printf("Cuantos numeros ingresaras: \n");
+    scanf("%d", &n);
+    if (n > limite)
+    {
+        do
+        {
+            system("CLS");
+            printf("Ingresa una cantidad valida\n");
+            system("PAUSE");
+            printf("Cuantos numeros ingresaras: \n");
+            scanf("%d", &n);
+        } while (n > limite);
+    }
+
+    i = 0;
+    numero = 0;
+    suma = 0;
+    for (i = 1; i <= n; i++, suma += numero)
+    {
+        printf("Numero %d: \n", i);
+        scanf("%d", &numero);
+    }
+    system("CLS");
+    printf("La suma de los numeros es: %d\n", suma);
+    printf("La media aritmetica de los numeros es: %.2f\n", ((suma + .0) / n));
     system("PAUSE");
 }
 //****************************
-void Digitos(void)
+void Embarcacion(void)
 {
     //  VARIALES LOCALES
+    int turistas, peso, band, ac;
     system("CLS");
     //  AQUI DESARROLLO PROGRAMA
-    printf("   DIGITOS\n");
+    printf("   EMBARCACION FINISTERRA\n");
+    band = 1;
+    turistas = 0;
+    ac=0;
+    while (band)
+    {
+        printf("Ingresa tu peso turista %d: \n", ++turistas);
+        scanf("%d", &peso);
+        ac += peso;
+        if (peso >= 805)
+        {
+            band = 0;
+        }
+        if (turistas == 10)
+        {
+            band = 0;
+        }
+    }
+    
+    printf("El promedio de peso de los pasajeros es: %.2f\n", (ac + .0) / (turistas + .0));
+
+    if (turistas == 10)
+    {
+        printf("Se llego al maximo de pasajeros permitidos\n");
+    }
+    else
+    {
+        printf("Se llego al maximo de peso permitido en la embarcacion\n");
+    }
     system("PAUSE");
 }
