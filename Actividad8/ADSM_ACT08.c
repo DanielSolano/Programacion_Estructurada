1ef1f10a4e171fbbe8d449775270de07cc318f0e
DanielSolano <solanomezaangel@gmail.com>
2023-09-30 14:13:17 -0700

    modified:   Actividad7/output/ADSM_ACT07_01.exe 	new file:   Actividad8/ADSM_ACT08.c 	new file:   Actividad8/output/ADSM_ACT08.exe

---
+++ Actividad8/ADSM_ACT08.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+//*** PROTOTIPOS DE FUNCIONES  ******
+int msges();
+void menu();
+void LlenarVector(int vect[], int m, int ri, int rf, int op);
+int Validar(int inferior, int superior);
+void Vector3(int vect3[], int vect2[], int vect[1]);
+void ImprimirVector(char msg[], int vect[], int m);
+void Matriz(void);
+void ImprimirMatriz(void);
+
+//****  main principal  *********
+int main()
+{
+    menu();
+
+    return 0;
+}
+//  *** DESARROLLO DE LAS FUNCIONES  ******
+//*********************
+int msges()
+{
+    int op;
+    system("CLS");
+    printf("   M  E   N   U \n");
+    printf("1.- LLENAR VECTOR 1 MANUALMENTE  \n");
+    printf("2.- LLENAR VECTOR 2 ALEATORIAMENTE \n");
+    printf("3.- LLENAR VECTOR 3 CON VECTOR 1 Y VECTOR 2 \n");
+    printf("4.- IMPRIMIR VECTORES \n");
+    printf("5.- LLENA MATRIZ 4 X 4 \n");
+    printf("6.- IMPRIMIR MATRIZ \n");
+    printf("0.- SALIR  \n");
+    printf("ESCOGE UNA OPCION: ");
+    scanf("%d", &op);
+    return op;
+}
+//****************
+void menu()
+{
+    int op, vector1[10], vector2[10], vector3[20];
+    do
+    {
+        srand(time(NULL));
+        op = msges();
+        switch (op)
+        {
+        case 1:
+            printf("LLENAR VECTOR 1 MANUALMENTE\n");
+            LlenarVector(vector1, 10, 30, 70, 0);
+            system("CLS");
+            printf("Vector 1 rellenado\n");
+            system("PAUSE");
+            break;
+        case 2:
+            printf("LLENAR VECTOR 2 AUTOMATICAMENTE\n");
+            LlenarVector(vector2, 10, 1, 20, 1);
+            system("CLS");
+            printf("Vector 2 rellenado\n");
+            system("PAUSE");
+            break;
+        case 3:
+            printf("LLENAR VECTOR 3 \n");
+            Vector3(vector3, vector1, vector2);
+            system("CLS");
+            printf("Vector 3 rellenado\n");
+            system("PAUSE");
+            break;
+        case 4:
+            ImprimirVector("VECTOR 1", vector1, 10);
+            ImprimirVector("VECTOR 2", vector2, 10);
+            ImprimirVector("VECTOR 3", vector3, 20);
+            break;
+        case 5:
+            Matriz();
+            break;
+        case 6:
+            ImprimirMatriz();
+            break;
+        }
+
+    } while (op != 0);
+}
+
+//*********************
+int Validar(char msg[], int inferior, int superior) // Parametros para funcionar
+{
+    int num;
+    char cadena[3];
+    do
+    {
+        printf("%s", msg);
+        fflush(stdin);
+        gets(cadena);       // Tomas los datos como cadena
+        num = atoi(cadena); // Transforma la entrada a entero
+    } while ((num < inferior) || (num > superior));
+    return num;
+}
+//*********************
+void LlenarVector(int vect[], int m, int ri, int rf, int op)
+{
+    //  VARIABLES LOCALES
+    int i;
+    system("CLS");
+    //  AQUI DESARROLLO PROGRAMA
+    if (op == 1)
+    {
+        for (i = 0; i < m; i++)
+        {
+            vect[i] = (rand() % (ri - rf)) + ri;
+        }
+    }
+    else
+    {
+        for (i = 0; i < m; i++)
+        {
+            printf("    Espacio [%d]\n", i + 1);
+            vect[i] = Validar("Numero entre 30 y 70\n", 30, 70);
+        }
+    }
+}
+
+//****************************
+void Vector3(int vect3[], int vect2[], int vect1[])
+{
+    //  VARIABLES LOCALES
+    int i;
+    system("CLS");
+    for (i = 0; i < 10; i++)
+    {
+        vect3[i] = vect2[i];
+        vect3[i + 10] = vect1[i];
+    }
+    //  AQUI DESARROLLO PROGRAMA
+}
+//****************************
+void ImprimirVector(char msg[], int vect[], int m)
+{
+    //  VARIALES LOCALES
+    int i;
+    system("CLS");
+    //  AQUI DESARROLLO PROGRAMA
+    printf("%s\n",msg);
+    for (i = 0; i < m ; i++)
+    {
+        printf("[%d] --> %d\n",i+1,vect[i]);
+    }
+    system("PAUSE");
+}
+//****************************
+void Matriz(void)
+{
+    //  VARIALES LOCALES
+    system("CLS");
+    //  AQUI DESARROLLO PROGRAMA
+    printf("   DIGITOS\n");
+    system("PAUSE");
+}
+//****************************
+void ImprimirMatriz(void)
+{`
+    //  VARIALES LOCALES
+    system("CLS");
+    //  AQUI DESARROLLO PROGRAMA
+    printf("   DIGITOS\n");
+    system("PAUSE");
+}
