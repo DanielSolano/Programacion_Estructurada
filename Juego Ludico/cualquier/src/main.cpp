dbdfd4f1a6bafe91117601a5a1a64feed53f618a
DanielSolano <solanomezaangel@gmail.com>
2023-11-20 23:35:06 -0800

    new file:   Actividad10/Actividad 10 Archivos/SMAD_RP10_ANEXO.pdf 	new file:   Actividad10/Actividad 10 Archivos/SMAD_RP10_PE.pdf 	new file:   Actividad11/Actividad 11 Archivos/SMAD_RP11_ANEXO.pdf 	new file:   Actividad11/Actividad 11 Archivos/SMAD_RP_RP11_PE.pdf 	new file:   Actividad12/Actividad 12 Archivos/SMAD_RP12_ANEXO.pdf 	new file:   Actividad12/Actividad 12 Archivos/SMAD_RP_RP12_PE.pdf 	new file:   Actividad13/Actividad 13 Archivos/SMAD_RP13_ANEXO.pdf 	new file:   Actividad13/Actividad 13 Archivos/SMAD_RP_RP13_PE.pdf 	new file:   Actividad3/Actividad 3 Archivos/SMAD_RP03_ANEXO.pdf 	new file:   Actividad3/Actividad 3 Archivos/SMAD_RP03_PE.pdf 	new file:   Actividad4/Actividad 4 Archivos/SMAD_RP04_ANEXO.pdf 	new file:   Actividad4/Actividad 4 Archivos/SMAD_RP04_PE.pdf 	new file:   Actividad5/Actividad 5 Archivos/SMAD_RP05_ANEXO.pdf 	new file:   Actividad5/Actividad 5 Archivos/SMAD_RP05_PE.pdf 	new file:   Actividad6/Actividad 6 Archivos/SMAD_RP06_ANEXO.pdf 	new file:   Actividad6/Actividad 6 Archivos/SMAD_RP06_PE.pdf 	new file:   Actividad7/Actividad 7 Archivos/SMAD_RP07_ANEXO.pdf 	new file:   Actividad7/Actividad 7 Archivos/SMAD_RP07_PE.pdf 	new file:   Actividad8/Actividad 8 Archivos/SMAD_RP08_ANEXO.pdf 	new file:   Actividad8/Actividad 8 Archivos/SMAD_RP08_PE.pdf 	new file:   Actividad9/Actividad 9 Archivos/SMAD_RP09_ANEXO.pdf 	new file:   Actividad9/Actividad 9 Archivos/SMAD_RP09_PE.pdf 	new file:   CURP/Actividad CURP Archivos/SMAD_RP_CURP_ANEXO.pdf 	new file:   CURP/Actividad CURP Archivos/SMAD_RP_CURP_PE.pdf 	deleted:    Juego Ludico/cualquier/src/elefante.png 	modified:   Juego Ludico/cualquier/src/main.cpp 	new file:   Taller/Taller2/Taller 2 Archivos/ADSM_TALLER2_ANEXO.pdf

--- Juego Ludico/cualquier/src/main.cpp
+++ Juego Ludico/cualquier/src/main.cpp
@@ -1,19 +1,95 @@
-
 #include "raylib.h"
+#define JUGAR 0
+#define OPCIONES 1
+#define SALIR 2
 
-int main()
+void menu(void);
+
+int main(void)
 {
-    Texture2D sprite;
-    InitWindow(800, 400, "basurita");
-    sprite = LoadTexture("elefante.png");
-    while (!WindowShouldClose())
+    int optionmenu=0;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
+    InitWindow(screenWidth, screenHeight, "window");
+    SetTargetFPS(60);        
+    Texture2D menu=LoadTexture("texturas\\menuok.png");
+    Texture2D car=LoadTexture("texturas\\pixel_car2.png");
+    Texture2D car2=LoadTexture("texturas\\pixel_car.png");
+    Texture2D sub=LoadTexture("texturas\\SUB.png");
+    while (!WindowShouldClose()) 
     {
+       
+        if(!IsWindowFullscreen())
+        {
+            ToggleFullscreen();
+        }
+
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
-        DrawText("Funciona por favor", 100, 100, 15, BLACK);
-        DrawCircle(400, 200, 50, BLACK);
-        DrawTexture(sprite, 10, 10, WHITE);
+        
+        DrawTexture(menu,0,0,WHITE);
+
+        if(IsKeyPressed(KEY_UP))
+        {
+            switch(optionmenu)
+            {
+                case OPCIONES:
+                    optionmenu=JUGAR;
+                    break;
+                case SALIR:
+                    optionmenu=OPCIONES;
+                    break;
+            }
+        }
+        else
+        {
+            if(IsKeyPressed(KEY_DOWN))
+            {
+                switch(optionmenu)
+                {
+                    case JUGAR:
+                        optionmenu=OPCIONES;
+                        break;
+                    case OPCIONES:
+                        optionmenu=SALIR;
+                        break;
+                }
+            }
+        }
+
+        DrawTexture(car, 800,300,WHITE);
+        DrawTexture(sub, 190, 200,WHITE);
+        DrawTexture(car2, 800, 100,WHITE);
+        switch(optionmenu)
+        {
+            case JUGAR:
+                DrawText("Mecánico Aritmético",400,70,50,WHITE);
+                DrawText("Jugar",510,200,60,WHITE);
+                DrawText("Opciones",510,300,40,WHITE);
+                DrawText("Salir",510,400,40,WHITE);
+                break;
+            case OPCIONES:
+                DrawText("Mecánico Aritmético",400,70,50,WHITE);
+                DrawText("Jugar",510,200,40,WHITE);
+                DrawText("Opciones",510,300,60,WHITE);
+                DrawText("Salir",510,400,40,WHITE);
+                break;
+            case SALIR:
+                DrawText("Mecánico Aritmético",400,70,50,WHITE);
+                DrawText("Jugar",510,200,40,WHITE);
+                DrawText("Opciones",510,300,40,WHITE);
+                DrawText("Salir",510,400,60,WHITE);
+                break;
+        }
+        //Original
+        //DrawText("Mecánico Aritmético",400,70,50,WHITE);
+        //DrawText("Jugar",510,200,40,WHITE);
+        //DrawText("Opciones",510,300,40,WHITE);
+        //DrawText("Salir",510,400,40,WHITE);
+        
         EndDrawing();
     }
+    CloseWindow();       
+
+    return 0;
 }
