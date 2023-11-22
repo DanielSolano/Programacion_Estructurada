f774cf0a2818c2582f209d016b7b82f57dac3586
DanielSolano <solanomezaangel@gmail.com>
2023-11-21 22:51:49 -0800

    modified:   Juego Ludico/cualquier/build/Game.exe 	new file:   Juego Ludico/cualquier/build/texturas/SUB.png 	new file:   Juego Ludico/cualquier/build/texturas/menuok.png 	new file:   Juego Ludico/cualquier/build/texturas/navepix2.png 	new file:   Juego Ludico/cualquier/build/texturas/pixel_car.png 	new file:   Juego Ludico/cualquier/build/texturas/pixel_car2.png 	modified:   Juego Ludico/cualquier/src/main.cpp

--- Juego Ludico/cualquier/src/main.cpp
+++ Juego Ludico/cualquier/src/main.cpp
@@ -1,95 +1,159 @@
 #include "raylib.h"
-#define JUGAR 0
-#define OPCIONES 1
-#define SALIR 2
 
-void menu(void);
+//------------------------------------------------------------------------------------------
+// Types and Structures Definition
+//------------------------------------------------------------------------------------------
+typedef enum GameScreen
+{
+    MENU = 0,
+    OPCIONES,
+    GAMEPLAY
+} GameScreen;
 
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
 int main(void)
 {
-    int optionmenu=0;
+    // Initialization
+    //--------------------------------------------------------------------------------------
     const int screenWidth = 1280;
     const int screenHeight = 720;
-    InitWindow(screenWidth, screenHeight, "window");
-    SetTargetFPS(60);        
-    Texture2D menu=LoadTexture("texturas\\menuok.png");
-    Texture2D car=LoadTexture("texturas\\pixel_car2.png");
-    Texture2D car2=LoadTexture("texturas\\pixel_car.png");
-    Texture2D sub=LoadTexture("texturas\\SUB.png");
-    while (!WindowShouldClose()) 
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic screen manager");
+
+    GameScreen currentScreen = MENU;
+
+    // TODO: Initialize all required variables and load all required data here!
+
+    int framesCounter = 0; // Useful to count frames
+
+    Texture2D volumen[7];
+    Texture2D dif[3];
+    Texture2D fondo = LoadTexture("texturas\\menuok.png");
+    volumen[0] = LoadTexture("texturas\\vol0.png");
+    volumen[1] = LoadTexture("texturas\\vol1.png");
+    volumen[2] = LoadTexture("texturas\\vol2.png");
+    volumen[3] = LoadTexture("texturas\\vol3.png");
+    volumen[4] = LoadTexture("texturas\\vol4.png");
+    volumen[5] = LoadTexture("texturas\\vol5.png");
+    volumen[6] = LoadTexture("texturas\\vol6.png");
+    dif[0] = LoadTexture("texturas\\dif1.png");
+    dif[1] = LoadTexture("texturas\\dif2.png");
+    dif[2] = LoadTexture("texturas\\dif3.png");
+
+    SetTargetFPS(60); // Set desired framerate (frames-per-second)
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-       
-        if(!IsWindowFullscreen())
+        // Update
+        //----------------------------------------------------------------------------------
+        switch (currentScreen)
         {
-            ToggleFullscreen();
-        }
-
-        BeginDrawing();
-
-        
-        DrawTexture(menu,0,0,WHITE);
-
-        if(IsKeyPressed(KEY_UP))
+        case MENU:
         {
-            switch(optionmenu)
+            // Press enter to change to GAMEPLAY screen
+            if (IsKeyPressed(KEY_DOWN))
             {
-                case OPCIONES:
-                    optionmenu=JUGAR;
-                    break;
-                case SALIR:
-                    optionmenu=OPCIONES;
-                    break;
-            }
-        }
-        else
-        {
-            if(IsKeyPressed(KEY_DOWN))
-            {
-                switch(optionmenu)
+                if (IsKeyPressed(KEY_ENTER))
                 {
-                    case JUGAR:
-                        optionmenu=OPCIONES;
-                        break;
-                    case OPCIONES:
-                        optionmenu=SALIR;
-                        break;
+                    currentScreen = OPCIONES;
+                }
+                
+                if (IsKeyPressed(KEY_DOWN))
+                {
+                    if (IsKeyPressed(KEY_ENTER))
+                    {
+                        CloseWindow();
+                    }
                 }
             }
         }
-
-        DrawTexture(car, 800,300,WHITE);
-        DrawTexture(sub, 190, 200,WHITE);
-        DrawTexture(car2, 800, 100,WHITE);
-        switch(optionmenu)
+        break;
+        case OPCIONES:
         {
-            case JUGAR:
-                DrawText("Mecánico Aritmético",400,70,50,WHITE);
-                DrawText("Jugar",510,200,60,WHITE);
-                DrawText("Opciones",510,300,40,WHITE);
-                DrawText("Salir",510,400,40,WHITE);
-                break;
-            case OPCIONES:
-                DrawText("Mecánico Aritmético",400,70,50,WHITE);
-                DrawText("Jugar",510,200,40,WHITE);
-                DrawText("Opciones",510,300,60,WHITE);
-                DrawText("Salir",510,400,40,WHITE);
-                break;
-            case SALIR:
-                DrawText("Mecánico Aritmético",400,70,50,WHITE);
-                DrawText("Jugar",510,200,40,WHITE);
-                DrawText("Opciones",510,300,40,WHITE);
-                DrawText("Salir",510,400,60,WHITE);
-                break;
+            // Press enter to change to GAMEPLAY screen
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentScreen = OPCIONES;
+            }
         }
-        //Original
-        //DrawText("Mecánico Aritmético",400,70,50,WHITE);
-        //DrawText("Jugar",510,200,40,WHITE);
-        //DrawText("Opciones",510,300,40,WHITE);
-        //DrawText("Salir",510,400,40,WHITE);
-        
+        break;
+        case GAMEPLAY:
+        {
+            // TODO: Update GAMEPLAY screen variables here!
+
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentScreen = MENU;
+            }
+        }
+        break;
+        default:
+            break;
+        }
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        switch (currentScreen)
+        {
+        case MENU:
+        {
+            // TODO: Draw LOGO screen here!
+            DrawTexture(fondo, 0, 0, WHITE);
+            DrawText("Mecánico Aritmético", 265, 65, 70, WHITE);
+            DrawText("Jugar", 510, 200, 60, WHITE);
+            DrawText("Opciones", 510, 300, 40, WHITE);
+            DrawText("Salir", 510, 400, 40, WHITE);
+        }
+        break;
+        case OPCIONES:
+        {
+            // TODO: Draw TITLE screen here!
+            DrawTexture(fondo, 0, 0, WHITE);
+            DrawText("Opciones", 450, 65, 70, WHITE);
+            DrawText("Dificultad", 510, 200, 60, WHITE);
+            DrawText("Sonido", 510, 350, 40, WHITE);
+            DrawText("Regresar", 510, 500, 40, WHITE);
+        }
+        break;
+        case GAMEPLAY:
+        {
+            // TODO: Draw GAMEPLAY screen here!
+            DrawTexture(fondo, 0, 0, WHITE);
+            DrawText("PROXIMAMENTE", 450, 65, 70, WHITE);
+        }
+        break;
+        default:
+            break;
+        }
+
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
-    CloseWindow();       
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+
+    // TODO: Unload all loaded data (textures, fonts, audio) here!
+    UnloadTexture(fondo);
+    UnloadTexture(volumen[0]);
+    UnloadTexture(volumen[1]);
+    UnloadTexture(volumen[2]);
+    UnloadTexture(volumen[3]);
+    UnloadTexture(volumen[4]);
+    UnloadTexture(volumen[5]);
+    UnloadTexture(volumen[6]);
+    UnloadTexture(dif[0]);
+    UnloadTexture(dif[1]);
+    UnloadTexture(dif[2]);
+    CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
